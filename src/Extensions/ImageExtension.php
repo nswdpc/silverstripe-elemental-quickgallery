e6e8730b3a5e235ade1fe93bb75831338bb41b30
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2025-12-10 14:42:07 +1100

    Initial update

--- src/Extensions/ImageExtension.php
+++ src/Extensions/ImageExtension.php
@@ -3,14 +3,14 @@
 namespace NSWDPC\Elemental\Extensions\QuickGallery;
 
 use NSWDPC\Elemental\Models\QuickGallery\ElementQuickGallery;
-use SilverStripe\ORM\DataExtension;
+use SilverStripe\Core\Extension;
 
 /**
  * Provide reverse association with galleries
  * @method \SilverStripe\ORM\ManyManyList<\NSWDPC\Elemental\Models\QuickGallery\ElementQuickGallery> QuickGalleries()
- * @extends \SilverStripe\ORM\DataExtension<static>
+ * @extends \SilverStripe\Core\Extension<static>
  */
-class ImageExtension extends DataExtension
+class ImageExtension extends Extension
 {
     private static array $belongs_many_many = [
         'QuickGalleries' => ElementQuickGallery::class
